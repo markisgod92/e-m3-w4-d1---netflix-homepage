fb6dec839a3a5eb003b9e401105a8235ade8a7d1
markisgod92 <markisgod92@gmail.com>
2024-07-09 00:08:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const movies = [
+    {
+        title: "Unorthodox",
+        img: "assets/imgs/movies/1.png",
+        category: "13+",
+        content: "Miniserie",
+        genre: ["profondo", "emozionante", "dramma"],
+    },
+    {
+        title: "After Life",
+        img: "assets/imgs/movies/2.png",
+        category: "16+",
+        content: "3 stagioni",
+        genre: ["Umorismo inglese", "Arguto", "Commedia cupa"]
+    },
+    {
+        title: "Vis a Vis: il Prezzo del Riscatto",
+        img: "assets/imgs/movies/3.png",
+        category: "16+",
+        content: "5 stagioni",
+        genre: ["Thriller", "LGBTQ", "Carcere"]
+    },
+    {
+        title: "The Place",
+        img: "assets/imgs/movies/4.png",
+        category: "13+",
+        content: "105 min",
+        genre: ["Drammatico", "Noir", "Fantastico"]
+    },
+    {
+        title: "Summertime",
+        img: "assets/imgs/movies/5.png",
+        category: "13+",
+        content: "3 stagioni",
+        genre: ["Stile soap", "Dolceamaro", "Adolescenziale"]
+    },
+    {
+        title: "I Delitti di Valhalla",
+        img: "assets/imgs/movies/6.png",
+        category: "16+",
+        content: "Miniserie",
+        genre: ["Cupo", "Suspense"]
+    },
+];
