06696bfed26143e6f35e3b81dfdcca1c4eabaf50
markisgod92 <markisgod92@gmail.com>
2024-07-09 18:24:25 +0200

    funzioni card

--- script.js
+++ script.js
@@ -1,68 +1,99 @@
 const movies = [
     {
         title: "Unorthodox",
-        img: "assets/imgs/movies/1.png",
+        img: `<img src="assets/imgs/movies/1.png" alt="unorthox">`,
+        embedVideo: `<iframe width="100%" height="100%" src="http://www.youtube.com/embed/SaqWJ-lQDtE?autoplay=1&mute=1&loop=1" 
+                        title="Unorthodox  - Trailer Ufficiale" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin"></iframe>`,
         category: "13+",
         content: "Miniserie",
-        genre: ["profondo", "emozionante", "dramma"],
+        tags: ["Profondo", "Emozionante", "Dramma"],
+        genre: "Drama"
     },
     {
         title: "After Life",
-        img: "assets/imgs/movies/2.png",
+        img: `<img src="assets/imgs/movies/2.png" alt="after life">`,
+        embedVideo: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Qad_RHzz9B8?autoplay=1&mute=1&loop=1" 
+                        title="AFTER LIFE | Trailer ITA Della Serie Netflix con Ricky Gervais" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
         category: "16+",
         content: "3 stagioni",
-        genre: ["Umorismo inglese", "Arguto", "Commedia cupa"]
+        tags: ["Umorismo inglese", "Arguto", "Commedia cupa"],
+        genre: ["Drama", "Comedy"]
     },
     {
         title: "Vis a Vis: il Prezzo del Riscatto",
-        img: "assets/imgs/movies/3.png",
+        img: `<img src="assets/imgs/movies/3.png" alt="vis a vis">`,
+        embedVideo: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/eqdaWny5Irs?autoplay=1&mute=1&loop=1" 
+                        title="Vis a Vis: El Oasis | Trailer ufficiale (sottotitoli in ITALIANO) | Netflix Italia" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
         category: "16+",
         content: "5 stagioni",
-        genre: ["Thriller", "LGBTQ", "Carcere"]
+        tags: ["Thriller", "LGBTQ", "Carcere"],
+        genre: ["Crime", "Thriller"]
     },
     {
         title: "The Place",
-        img: "assets/imgs/movies/4.png",
+        img: `<img src="assets/imgs/movies/4.png" alt="the place">`,
+        embedVideo: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/RDQwNVp65HY?autoplay=1&mute=1&loop=1" 
+                        title="The place - Trailer ufficiale" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
         category: "13+",
         content: "105 min",
-        genre: ["Drammatico", "Noir", "Fantastico"]
+        tags: ["Drammatico", "Noir", "Fantastico"],
+        genre: ["Drama", "Thriller"]
     },
     {
         title: "Summertime",
-        img: "assets/imgs/movies/5.png",
+        img: `<img src="assets/imgs/movies/5.png" alt="summertime">`,
+        embedVideo: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/n1yLsV-IPOk?autoplay=1&mute=1&loop=1" 
+                        title="Summertime | Trailer ufficiale | Netflix Italia" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
         category: "13+",
         content: "3 stagioni",
-        genre: ["Stile soap", "Dolceamaro", "Adolescenziale"]
+        tags: ["Stile soap", "Dolceamaro", "Adolescenziale"],
+        genre: ["Romantic", "Drama"]
     },
     {
         title: "I Delitti di Valhalla",
-        img: "assets/imgs/movies/6.png",
+        img: `<img src="assets/imgs/movies/6.png" alt="i delitti di valhalla">`,
+        embedVideo: `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/pQyy7DJtwEI?autoplay=1&mute=1&loop=1" 
+                        title="Valhalla Murders: Trailer | BBC Trailers" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
         category: "16+",
         content: "Miniserie",
-        genre: ["Cupo", "Suspense"]
+        tags: ["Cupo", "Suspense"],
+        genre: ["Drama", "Thriller"]
     },
 ];
 
 const footerLinks = [
-    {title: "Audio and Subtitles", link: "#"},
-    {title: "Audio Description", link: "#"},
-    {title: "Help Center", link: "#"},
-    {title: "Gift Cards", link: "#"},
-    {title: "Media Center", link: "#"},
-    {title: "Investor Relations", link: "#"},
-    {title: "Jobs", link: "#"},
-    {title: "Terms of Use", link: "#"},
-    {title: "Privacy", link: "#"},
-    {title: "Legal Notices", link: "#"},
-    {title: "Cookie Preferences", link: "#"},
-    {title: "Corporate Information", link: "#"},
-    {title: "Contact Us", link: "#"}
+    { title: "Audio and Subtitles", link: "#" },
+    { title: "Audio Description", link: "#" },
+    { title: "Help Center", link: "#" },
+    { title: "Gift Cards", link: "#" },
+    { title: "Media Center", link: "#" },
+    { title: "Investor Relations", link: "#" },
+    { title: "Jobs", link: "#" },
+    { title: "Terms of Use", link: "#" },
+    { title: "Privacy", link: "#" },
+    { title: "Legal Notices", link: "#" },
+    { title: "Cookie Preferences", link: "#" },
+    { title: "Corporate Information", link: "#" },
+    { title: "Contact Us", link: "#" }
 ]
 
 
 // DOM Elements
 const showCategoriesBtn = document.getElementById("show-sections");
 const showGridBtn = document.getElementById("show-grid");
+const showsCointaner = document.getElementById("shows-container");
 const footerLinksContainer = document.getElementById("footer-links-container");
 
 // Functions
@@ -79,27 +110,77 @@ const createLink = (link, container) => {
     container.appendChild(linkLi);
 }
 
+const createCard = (show, container) => {
+    const showWrapper = document.createElement("div");
+    showWrapper.setAttribute("class", "show-card position-relative");
+
+    const showCard = document.createElement("div");
+    showCard.setAttribute("class", "card bg-black");
+
+    const cardImg = document.createElement("div");
+    cardImg.setAttribute("class", "card-img-top");
+    cardImg.innerHTML = show.img;
+
+    showCard.appendChild(cardImg);
+
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+
+    createCardBody(show, cardBody)
+
+    showCard.append(cardImg, cardBody);
+    showWrapper.appendChild(showCard);
+    container.appendChild(showWrapper);
+
+    let timerID;
+    showWrapper.addEventListener("mouseenter", () => {
+        timerID = setTimeout(() => {
+            cardImg.innerHTML = show.embedVideo;
+        })
+    })
+    showWrapper.addEventListener("mouseleave", () => {
+        clearTimeout(timerID);
+        cardImg.innerHTML = show.img;
+    })
+
+}
+
+const createCardBody = (show, container) => {
+    const cardButtons = document.createElement("div");
+    cardButtons.setAttribute("class", "mb-3 d-flex align-items-center gap-2");
+    cardButtons.innerHTML = `<i class="bi bi-play-circle-fill text-white fs-1"></i>
+                                <i class="bi bi-plus-circle text-white fs-1"></i>
+                                <i class="bi bi-hand-thumbs-up text-white fs-1"></i>`;
+
+    const cardData = document.createElement("div");
+    cardData.setAttribute("class", "mb-3 d-flex align-items-center gap-3");
+
+    const showCategory = document.createElement("div");
+    showCategory.setAttribute("class", "border border-secondary border-1 px-1 text-secondary");
+    showCategory.innerText = show.category;
+
+    const showContent = document.createElement("div");
+    showContent.setAttribute("class", "text-secondary");
+    showContent.innerText = show.content;
+
+    cardData.append(showCategory, showContent);
+
+    const cardTags = document.createElement("ul");
+    cardTags.setAttribute("class", "d-flex justify-content-start align-items-center gap-5 text-white list-unstyled");
+
+    show.tags.forEach(tag => {
+        const cardLi = document.createElement("li");
+        cardLi.innerText = tag;
+        cardTags.appendChild(cardLi);
+    })
+
+    container.append(cardButtons, cardData, cardTags)
+}
+
+
 // Calls
+movies.forEach(movie => {
+    createCard(movie, showsCointaner);
+})
+
 footerLinks.forEach(link => createLink(link, footerLinksContainer));
-
-
-// TEST CARD
-let timerID;
-const showCard = document.querySelector(".card");
-const showImg = document.querySelector(".card-img-top");
-const showBody = document.querySelector(".card-body");
-
-showCard.addEventListener("mouseover", () => {
-    timerID = setTimeout(() => {
-        showImg.innerHTML = `<iframe class="card-img-top" width="100%" height="100%" src="http://www.youtube.com/embed/SaqWJ-lQDtE?autoplay=1&mute=1&loop=1" 
-                                title="Unorthodox  - Trailer Ufficiale" frameborder="0" 
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                                referrerpolicy="strict-origin-when-cross-origin"></iframe>`
-    }, 1000)
-    
-})
-
-showCard.addEventListener("mouseout", () => {
-    clearTimeout(timerID);
-    showImg.innerHTML = `<img src="assets/imgs/movies/1.png" alt="show pic">`
-})
